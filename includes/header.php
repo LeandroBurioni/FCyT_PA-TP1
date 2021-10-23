9cbc5f375bf424a585dd1c90ea0b47137b02f46d
LeandroBurioni <LeandroBurioni@gmail.com>
2021-10-23 13:12:39 -0300

    Reunion 23Oct

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
         <title>Vitreaux: Feria Online</title>
         <link rel="icon" type="image/png" href="./imagenes/favicon.png">
 		<link rel="stylesheet" href="./css/estilos.css">
-        
+
         <!-- Bootstrap CSS & js -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -17,24 +17,24 @@
 
     <body>
 	 <!-- Barra de navegación -->
-<nav class="navbar navbar-expand-md navbar-dark">
+<nav class="navbar navbar-expand-lg navbar-dark">
 
     <?php
 
      if(isset($_SESSION['usuario'])){        
         echo '
         <a class="navbar-brand" href="./inicio.php">
-            <img src="./imagenes/Logo3.png" alt="" width="200" height="auto" class="d-inline-block align-center">
+            <img src="./imagenes/Logo3.png" alt="Logo Feria Online" width="200" height="auto" class="d-inline-block align-center">
         </a>';
     }
     else{
         echo '
         <a class="navbar-brand" href="./index.php">
-            <img src="./imagenes/Logo3.png" alt="" width="200" height="auto" class="d-inline-block align-center">
+            <img src="./imagenes/Logo3.png" alt="Logo Feria Online" width="200" height="auto" class="d-inline-block align-center">
         </a>';
     }
     ?>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
     <div class="collapse navbar-collapse" id="navbarNav">
